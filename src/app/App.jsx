c798432e2084c52651ffaaadfa88ad91179f6254
sngff4821-beep <sngff4821@gmail.com>
2026-03-05 23:42:05 +0700

    login check teacher

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes'
-
+import Login from '../services/Login'
 function App() {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      {/* <AppRoutes /> */}
+      <Login/>
     </BrowserRouter>
   )
 }
