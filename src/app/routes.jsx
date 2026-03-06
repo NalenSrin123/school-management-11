23df6d2e3c32a5b42e9a1839d0310ad54094d57b
MenghoungChum <menghoung262006@gmail.com>
2026-03-06 09:09:59 +0700

    Design Register page

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Home from '../public-site/pages/Home'
 import Login from '../dashboard/pages/auth/Login'
+import Register from '../services/Register'
 
 function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ function AppRoutes() {
       <Route path='/' element={<Home />} />
       <Route path='/admin/login' element={<Login />} />
       <Route path='*' element={<Navigate to='/' replace />} />
+      <Route path='/login' element={<Register/>}></Route>
     </Routes>
   )
 }
