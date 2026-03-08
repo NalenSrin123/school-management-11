23038771d1047057784e0c9763d76d7d33979f05
unknown <piseysan39367@gmail.com>
2026-03-08 09:54:03 +0700

    design new curriculum

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes'
+import Courses from '../public-site/pages/Courses'
 
 function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
-      <h1 className='text-amber-200'>Hello World</h1>
-      <h1>Hello</h1>
+      <Courses/>
+
     </BrowserRouter>
   )
 }
