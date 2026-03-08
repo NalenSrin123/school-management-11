8947a205589456a0d1e77049f2131b27373e479e
NalenSrin123 <srinnalen@gmail.com>
2026-03-08 12:09:38 +0700

    Merge to app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,34 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes'
 import Courses from '../public-site/pages/Courses'
+import Register from '../services/Register';
+import ConfirmOTPpage from '../public-site/pages/ConfoirmOTPpage';
+import ResetPassword from '../public-site/pages/ResetPassword';
+import Design_OTP_Page from '../public-site/pages/Design_OTP_Page';
+import KruInternship from '../public-site/pages/Internship';
+import Position from '../public-site/pages/Position';
+import Login from '../services/Login';
+import Footer from './Footer';
+import HeroSection from '../public-site/components/HeroSection';
+import CourseCard from '../public-site/components/CourseCard';
 
 function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
       <Courses/>
+      <Register/>
+      <ConfirmOTPpage/>
+      <ResetPassword/>
+      <Design_OTP_Page/>
+      <KruInternship/>
+      <Position/>
+      <Login/>
+      <Footer/>
+      <HeroSection/>
+      {/* <CourseCard/> */}
     </BrowserRouter>
+   
   );
 }
 
