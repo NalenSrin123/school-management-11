5ad00c98d6d6cb5d7d7775899410b87f7c2eac1c
sengcheav574-hue <sengcheav574@gmail.com>
2026-03-08 11:49:12 +0700

    Reset Password

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter } from 'react-router-dom'
-import AppRoutes from './routes'
-import Reset_password from '../public-site/pages/Reset_password'
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
+import Reset_password from "../public-site/pages/Reset_password";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <AppRoutes />
       <Reset_password />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/public-site/pages/Reset_password.jsx
+++ src/public-site/pages/Reset_password.jsx
@@ -5,8 +5,8 @@ import { MdError } from "react-icons/md";
 
 function Reset_password() {
   return (
-    <div className="flex justify-center items-center h-screen ">
-      <div className="w-150 bg-white shadow-xl shadow-gray-300 h-150 flex flex-col rounded-md ">
+    <div className="flex justify-center items-center  h-screen ">
+      <div className="w-150 bg-white shadow-md shadow-gray-400  h-154 flex flex-col rounded-xl ">
         <div className="py-5 px-10">
           <div className="flex justify-center">
             <img
@@ -35,7 +35,7 @@ function Reset_password() {
 
           {/* Button reset_password */}
           <div className="flex justify-center  w-full mt-5 ">
-            <button className="flex justify-center items-center gap-2 bg-blue-700 text-white rounded-md py-2 px-7 text-sm font-bold">
+            <button className="flex justify-center items-center gap-2 bg-blue-700 text-white rounded-md py-2 px-7 text-sm font-bold hover:shadow-blue-400 hover:shadow-md hover:scale-105 transform transition">
               Reset Password <FaArrowRightLong className="text-sm" />
             </button>
           </div>
@@ -60,7 +60,7 @@ function Reset_password() {
           </div>
         </div>
 
-        <div className="flex flex-col items-center mt-2 bg-gray-50 ">
+        <div className="flex flex-col items-center mt-2 bg-gray-50 rounded-b-xl ">
             <p className="text-xs p-2">
               Need help? Contact our support team at{" "}
               <span className="text-blue-500">support@eteccenter.com</span>
