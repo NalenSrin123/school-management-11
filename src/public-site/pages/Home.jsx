45ef327321b745d9e451be670cf92d15c01e951f
MenghoungChum <menghoung262006@gmail.com>
2026-03-12 19:10:22 +0700

    git manage Layout

--- src/public-site/pages/Home.jsx
+++ src/public-site/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Footer from "../../app/Footer";
 import HeroSection from "../components/HeroSection";
 import Navbar from "../layout/Navbar";
+import CoursesCard from "./CoursesCard";
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div className="h-auto w-full overflow-hidden">
       <Navbar />
       <HeroSection />
+      <CoursesCard/>
       <Footer/>
     </div>
   );
