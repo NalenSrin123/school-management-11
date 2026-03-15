6a06797d4ede00962800ebd7d117ec8b471adfb5
sengcheav574-hue <sengcheav574@gmail.com>
2026-03-15 11:45:01 +0700

    fix reset_password in email can responsive all device

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
 import ResetPassword from "../public-site/pages/ResetPassword";
+import Reset_password from "../public-site/pages/Reset_password";
 
 function App() {
   return (
     <BrowserRouter>
       <ResetPassword />
+      <Reset_password />
     </BrowserRouter>
   );
 }
