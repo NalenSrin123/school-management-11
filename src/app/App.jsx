956e95890b7e78b2de3db88c6556ab90b8ac2192
NalenSrin123 <srinnalen@gmail.com>
2026-03-15 12:05:07 +0700

    Last

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,7 +4,6 @@ import AppRoutes from './routes'
 import About from '../public-site/pages/About'
 
 import Register from '../services/Register';
-import ConfirmOTPpage from '../public-site/pages/ConfoirmOTPpage';
 import ResetPassword from '../public-site/pages/ResetPassword';
 import Design_OTP_Page from '../public-site/pages/Design_OTP_Page';
 import KruInternship from '../public-site/pages/Internship';
--- src/public-site/pages/About.jsx
+++ src/public-site/pages/About.jsx
@@ -1,9 +1,3 @@
-function About() {
-  return <div>
-    About
-    </div>
-}
-
 export default function About() {
   const [show, setShow] = useState(false);
 
