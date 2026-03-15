a46d989580492ede4bea040cf38b1c7b3c23ce53
Sihak2025 <caoyanbing739@gmail.com>
2026-03-15 12:28:46 +0700

    Update prepare change page

--- src/public-site/layout/Navbar.jsx
+++ src/public-site/layout/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 // ==================== CONFIRM OTP PAGE ====================
 const ConfirmOTPpage = ({ onBack, onClose }) => {
@@ -283,13 +284,13 @@ const Navbar = () => {
           </div>
 
           <ul className="hidden lg:flex items-center gap-6 text-[11px] font-bold uppercase tracking-widest">
-            <li className="text-[#FF9800] cursor-pointer hover:opacity-80 transition">Home</li>
-            <li className="text-[#0D47A1] cursor-pointer hover:text-blue-600 transition">About Us</li>
-            <li className="text-[#0D47A1] cursor-pointer hover:text-blue-600 transition">Internship</li>
-            <li className="text-[#0D47A1] cursor-pointer hover:text-blue-600 transition">Donate</li>
-            <li className="text-[#0D47A1] cursor-pointer hover:text-blue-600 transition flex items-center gap-1">
+            <Link to='/'><li className="text-[#FF9800] cursor-pointer hover:opacity-80 transition">Home</li></Link>
+            <Link to='/about'><li className="text-[#0D47A1] cursor-pointer hover:text-blue-600 transition">About Us</li></Link>
+            <Link to='/kruinternship'><li className="text-[#0D47A1] cursor-pointer hover:text-blue-600 transition">Internship</li></Link>
+            <Link to='/donate'><li className="text-[#0D47A1] cursor-pointer hover:text-blue-600 transition">Donate</li></Link>
+            <Link to='/coursecard'><li className="text-[#0D47A1] cursor-pointer hover:text-blue-600 transition flex items-center gap-1">
               Our Courses <span className="text-[10px]">▼</span>
-            </li>
+            </li></Link>
             <li
               onClick={() => setModal("login")}
               className="text-[#0D47A1] cursor-pointer hover:text-blue-600 transition normal-case"
