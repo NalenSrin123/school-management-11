167e895e96be8ec54f38a8f7a429e6ae622d8fab
MenghoungChum <menghoung262006@gmail.com>
2026-04-05 02:03:16 +0700

    prcoess fetch create update delete api banner

---
+++ src/services/bannerService.js
@@ -0,0 +1,68 @@
+import { faTrophy } from "@fortawesome/free-solid-svg-icons";
+import api from "./api"
+
+export const GetBanner=async ()=>{
+    try {
+        const response= await api.get('/banner');
+        const banners=response.data.data;
+
+        if(response.status==200){
+            return banners
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const createBanner= async (data)=>{
+    try {
+        const response= await api.post('/banner',data,{
+            headers: {
+                'Content-Type' :"multipart/form-data",
+            }
+        })
+        if(response.status==201){
+            return {
+                success: true,
+                message: response.data.message,
+                data: response.data.data
+            }
+            GetBanner()
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getBannerById= async (id)=>{
+    try {
+        const response=await api.get(`/banner/${id}`);
+        if(response.status==200){
+            return {
+                success: true,
+                data: response.data.data
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const updateBanner= async (id,data)=>{
+    try {
+        const respose= await api.put(`/banner/${id}`,data,{
+            headers:{
+                'Content-Type' : "multipart/form-data"
+            }
+        })
+
+        if(respose.status==200){
+            return {
+                success:true,
+                data: respose.data.data,
+                message: respose.data.message
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
