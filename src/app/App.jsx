17fad97c1359f218007d74e168f09e0fd20c6fef
NalenSrin123 <srinnalen@gmail.com>
2026-04-05 12:30:10 +0700

    Merge

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -34,14 +34,14 @@ import ListEvent from '../dashboard/pages/event/Listevent';
 import FormListLogo from '../public-site/components/FormListLogo';
 
 import Table from '../public-site/pages/User_table_list';
-import RouteMap from '../public-site/components/RouteMap';
+// import RouteMap from '../public-site/components/RouteMap';
 
 
 
 function App() {
   return (
     <BrowserRouter>
-    <RouteMap />
+    {/* <RouteMap /> */}
       <AppRoutes />
     </BrowserRouter>
   );
